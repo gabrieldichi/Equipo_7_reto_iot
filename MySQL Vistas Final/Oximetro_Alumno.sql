2c7fe5311f132aa8f7c86f3188652f6a1ddcf48b
JavierCoronaA01023063 <57466810+JavierCoronaA01023063@users.noreply.github.com>
2020-11-23 17:24:22 -0600

    Add files via upload

---
+++ MySQL Vistas Final/Oximetro_Alumno.sql
@@ -0,0 +1,52 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: Oximetro
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Alumno`
+--
+
+DROP TABLE IF EXISTS `Alumno`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Alumno` (
+  `Matricula` varchar(9) NOT NULL,
+  `Nombre` varchar(12) DEFAULT NULL,
+  `Apellido` varchar(12) DEFAULT NULL,
+  PRIMARY KEY (`Matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Alumno`
+--
+
+LOCK TABLES `Alumno` WRITE;
+/*!40000 ALTER TABLE `Alumno` DISABLE KEYS */;
+INSERT INTO `Alumno` VALUES ('A1568906','Lulu','Riddle'),('A2044969','Ian','Bros'),('A3256647','Mario','Riddle'),('A4420443','Mario','Yamamoto'),('A6445629','Mario','Riddle'),('A6613785','Tom','Bros'),('A7261962','Ian','Kohn'),('A816959','Octavio','Yamamoto'),('A9677824','Ian','Corona'),('A9859646','Luis','Corona');
+/*!40000 ALTER TABLE `Alumno` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-23 17:23:06
---
+++ MySQL Vistas Final/Oximetro_routines.sql
@@ -0,0 +1,93 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: Oximetro
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Temporary view structure for view `vista2`
+--
+
+DROP TABLE IF EXISTS `vista2`;
+/*!50001 DROP VIEW IF EXISTS `vista2`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `vista2` AS SELECT 
+ 1 AS `Nombre`,
+ 1 AS `Apellido`,
+ 1 AS `Nive. Oxigeno`,
+ 1 AS `Fecha de prueba`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Temporary view structure for view `vista1`
+--
+
+DROP TABLE IF EXISTS `vista1`;
+/*!50001 DROP VIEW IF EXISTS `vista1`*/;
+SET @saved_cs_client     = @@character_set_client;
+/*!50503 SET character_set_client = utf8mb4 */;
+/*!50001 CREATE VIEW `vista1` AS SELECT 
+ 1 AS `Nombre`,
+ 1 AS `Apellido`,
+ 1 AS `Ritmo Cardiaco`,
+ 1 AS `Fecha de prueba`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `vista2`
+--
+
+/*!50001 DROP VIEW IF EXISTS `vista2`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vista2` AS select `alumno`.`Nombre` AS `Nombre`,`alumno`.`Apellido` AS `Apellido`,`historial_oxigeno`.`Nivel_oxigeno` AS `Nive. Oxigeno`,`historial_oxigeno`.`Fecha` AS `Fecha de prueba` from (`alumno` join `historial_oxigeno` on((`alumno`.`Matricula` = `historial_oxigeno`.`Matricula`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+
+--
+-- Final view structure for view `vista1`
+--
+
+/*!50001 DROP VIEW IF EXISTS `vista1`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8mb4 */;
+/*!50001 SET character_set_results     = utf8mb4 */;
+/*!50001 SET collation_connection      = utf8mb4_0900_ai_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `vista1` AS select `alumno`.`Nombre` AS `Nombre`,`alumno`.`Apellido` AS `Apellido`,`historial_cardiaco`.`Ritmo_cardiaco` AS `Ritmo Cardiaco`,`historial_cardiaco`.`Fecha` AS `Fecha de prueba` from (`alumno` join `historial_cardiaco` on((`alumno`.`Matricula` = `historial_cardiaco`.`Matricula`))) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-11-23 17:23:09

83fea139300ca4f4c97b151ed308874bc9e9ed7e
JavierCoronaA01023063 <57466810+JavierCoronaA01023063@users.noreply.github.com>
2020-12-03 11:44:00 -0600

    Add files via upload

---
+++ Mysql Entrega Final/Oximetro_Historial_cardiaco.sql
@@ -0,0 +1,53 @@
+-- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+--
+-- Host: localhost    Database: Oximetro
+-- ------------------------------------------------------
+-- Server version	8.0.21
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Historial_cardiaco`
+--
+
+DROP TABLE IF EXISTS `Historial_cardiaco`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Historial_cardiaco` (
+  `Ritmo_cardiaco` float DEFAULT NULL,
+  `Matricula` varchar(9) DEFAULT NULL,
+  `Fecha` varchar(10) DEFAULT NULL,
+  KEY `FK_Matricula_idx` (`Matricula`),
+  CONSTRAINT `FK_Matricula` FOREIGN KEY (`Matricula`) REFERENCES `Alumno` (`Matricula`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Historial_cardiaco`
+--
+
+LOCK TABLES `Historial_cardiaco` WRITE;
+/*!40000 ALTER TABLE `Historial_cardiaco` DISABLE KEYS */;
+INSERT INTO `Historial_cardiaco` VALUES (90.877,'A01023063','29-09-2020');
+/*!40000 ALTER TABLE `Historial_cardiaco` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-12-03 11:43:34
